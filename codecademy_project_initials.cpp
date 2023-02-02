885d4b42906d9cc54a733bd3e869ae1f6fa7d4e6
Steve Mc.Touring <rahimi.saeed.gm@gmail.com>
2023-02-02 17:24:12 +0330

    writing initials with cpp

---
+++ codecademy_project_initials.cpp
@@ -0,0 +1,13 @@
+// my initials are S and R. This was a fun fact!
+#include <iostream>
+
+int main()
+{
+    std::cout << " SSS  RRRR \n";
+    std::cout << "S   S R   R\n";
+    std::cout << "S     R   R\n";
+    std::cout << " SSS  RRRR \n";
+    std::cout << "    S R R  \n";
+    std::cout << "S   S R  R \n";
+    std::cout << " SSS  R   R\n";
+}
