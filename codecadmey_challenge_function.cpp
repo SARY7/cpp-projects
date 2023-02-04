26eceb0860779c43ef21513f831d6f13ee605e39
Steve Mc.Touring <rahimi.saeed.gm@gmail.com>
2023-02-04 10:35:14 +0330

    use vector in a function

--- codecadmey_challenge_function.cpp
+++ codecadmey_challenge_function.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 
 void introduction(std::string first_name, std::string last_name)
 {
@@ -13,13 +14,18 @@ double average(double num1, double num2)
     return ave;
 }
 
-int thenth_power(int num)
+int tenth_power(int num)
 {
     return pow(num, 10);
 }
 
-std::vector first_three_multiples(int num)
+std::vector<int> first_three_multiples(int num)
 {
+    std::vector<int> multiple{};
+    multiple.push_back(num * 1);
+    multiple.push_back(num * 2);
+    multiple.push_back(num * 3);
+    return multiple;
 }
 
 int main()
@@ -29,5 +35,15 @@ int main()
     std::cout << "\n"
               << average(42, 24) << "\n";
 
-    std::cout << thenth_power(2) << "\n";
+    std::cout << tenth_power(2) << "\n";
+
+    for (int element : first_three_multiples(8))
+    {
+        std::cout << element << "\n";
+    }
+
+    for (int i = 0; i < first_three_multiples(8).size(); i++)
+    {
+        std::cout << first_three_multiples(8)[i] << "\n";
+    }
 }
