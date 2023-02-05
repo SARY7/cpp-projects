b19df1a1624cb2038fb1279d0d8c89975a16a85c
Steve Mc.Touring <rahimi.saeed.gm@gmail.com>
2023-02-05 11:23:39 +0330

    ufo game from codecademy
    writing a UFO game with functions.

---
+++ codecademy_ufo_function.hpp
@@ -0,0 +1,6 @@
+#include <vector>
+
+void greet();
+void display_status(std::vector<char> incorrect, std::string answer);
+void end_game(std::string answer, std::string codeword);
+void display_misses(int miss);
